120f1da75dbc9c5216f7bb40150660cbde076da1
Simon Dutil <sdutil@lcs.on.ca>
2018-11-23 13:54:29 -0500

    line art 2 almost done

--- Playground/Playground.playground/Pages/starting Code for Turtle Graphics.xcplaygroundpage/Contents.swift
+++ Playground/Playground.playground/Pages/starting Code for Turtle Graphics.xcplaygroundpage/Contents.swift
@@ -3,14 +3,13 @@ import PlaygroundSupport
 import TortoiseGraphics
 import CoreGraphics
 
-let myFrame = CGRect(x: 0, y: 0, width: 300, height: 300)
+let myFrame = CGRect(x: 0, y: 0, width: 500, height: 500)
 let canvas = PlaygroundCanvas(frame:myFrame)
-canvas.frameRate = 50
+canvas.frameRate = 300
 canvas.color = .white
 PlaygroundPage.current.liveView = canvas
 
 // Start drawing
-canvas.drawing { turtle in
+canvas.drawing { t in
 
-    
 }
