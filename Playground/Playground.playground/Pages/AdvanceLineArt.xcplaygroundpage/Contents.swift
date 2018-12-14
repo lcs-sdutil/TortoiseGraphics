e41de7537a3ffd4e03def8df06c11ac91ccf033d
Simon Dutil <sdutil@lcs.on.ca>
2018-12-14 13:36:04 -0500

    first mountain done

--- Playground/Playground.playground/Pages/AdvanceLineArt.xcplaygroundpage/Contents.swift
+++ Playground/Playground.playground/Pages/AdvanceLineArt.xcplaygroundpage/Contents.swift
@@ -17,11 +17,33 @@ canvas.drawing { t in
     t.hill()
     t.jump()
     t.rail()
-    t.penSize(1)
-    t.setPos(-35, 200)
-    t.setH(70)
     
-
+    // pensize smaller for far away mountains
+    t.penSize(2)
+    //find X and Y
+    t.xcor
+    t.ycor
+    // move to left of canvas
+    t.penUp()
+    t.setH(270)
+    t.fd(574)
+    //turn right
+    t.rt(90)
+    // up
+    t.fd(240)
+    //pendown
+    t.penDown()
+    t.setH(45)
+    t.fd(150)
+    //backdown
+    t.right(110)
+    t.fd(70)
+    t.lt(80)
+    t.fd(15)
+    t.right(80)
+    t.fd(90)
+    // next mountain
+    
 
     
     
